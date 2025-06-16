0d78918db3ed08f0019f824ccd4c131ae9de5e1e
StevenelKhoury <steven.devek@gmail.com>
2025-06-16 11:22:49 +0200

    initializing project

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/test")
+    public String home() {
+        return "Hello, Application is running!";
+    }
+}
